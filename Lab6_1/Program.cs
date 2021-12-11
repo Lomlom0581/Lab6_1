05b6a13786421e378a5345c58e5f1fe66c2a423a
Антон Еремеев <kzanton_81@mail.ru>
2021-12-12 01:10:41 +0300

    Добавьте файлы проекта.

---
+++ Lab6_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Ввести с клавиатуры предложение.
+Предложение представляет собой слова, разделенные пробелом.
+Знаки препинания не используются.
+Найти самое длинное слово в строке");
+            Console.WriteLine("");
+
+            Console.Write("Введите предложение: ");
+            string str = Console.ReadLine();
+
+           
+            var strings = str.Split(' ');
+            
+            string LongestWord = "";
+            
+            foreach (var s in strings)
+                 
+                if (s.Length > LongestWord.Length) LongestWord = s;
+
+            Console.WriteLine($"Cамое длинное слово: '{LongestWord}'");
+
+
+            Console.ReadKey();
+        }
+    }
+}
